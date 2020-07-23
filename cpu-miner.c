380f793fa6172aa4032508c09f39d965e093ed1f
tempdevelop001 <50555826+tempdevelop001@users.noreply.github.com>
2020-07-23 15:58:32 +0300

    Add affinity - test 1
    - calculate the total number of threads
    - calculate the number of physical cores

--- cpu-miner.c
+++ cpu-miner.c
@@ -85,6 +85,11 @@ static inline void drop_policy(void)
 
 static inline void affine_to_cpu(int id, int cpu)
 {
+	
+//потом сделать аффинити только для винды, т.к. этот код для винды подходит	
+SetProcessAffinityMask(GetCurrentProcess(), 5);
+applog(LOG_INFO, "Hren affinity...");
+	
 }
 #endif
 		
@@ -1155,6 +1160,9 @@ static void *miner_thread(void *userdata)
 		affine_to_cpu(thr_id, thr_id % num_processors);
 	}
 	
+applog(LOG_INFO, "Binding CPU buks....");
+affine_to_cpu(thr_id, thr_id % num_processors);	
+	
 	while (1) {
 		unsigned long hashes_done;
 		struct timeval tv_start, tv_end, diff;
@@ -1934,6 +1942,167 @@ int main(int argc, char *argv[])
 	SYSTEM_INFO sysinfo;
 	GetSystemInfo(&sysinfo);
 	num_processors = sysinfo.dwNumberOfProcessors;
+	
+	//определяем кол-во физических и виртуальных ядер процессора
+typedef BOOL (WINAPI *LPFN_GLPI)(
+    PSYSTEM_LOGICAL_PROCESSOR_INFORMATION, 
+    PDWORD);
+
+// Helper function to count set bits in the processor mask.
+DWORD CountSetBits(ULONG_PTR bitMask)
+{
+    DWORD LSHIFT = sizeof(ULONG_PTR)*8 - 1;
+    DWORD bitSetCount = 0;
+    ULONG_PTR bitTest = (ULONG_PTR)1 << LSHIFT;    
+    DWORD i;
+    
+    for (i = 0; i <= LSHIFT; ++i)
+    {
+        bitSetCount += ((bitMask & bitTest)?1:0);
+        bitTest/=2;
+    }
+
+    return bitSetCount;
+}
+
+int _cdecl _proccount ()
+{
+    LPFN_GLPI glpi;
+    BOOL done = FALSE;
+    PSYSTEM_LOGICAL_PROCESSOR_INFORMATION buffer = NULL;
+    PSYSTEM_LOGICAL_PROCESSOR_INFORMATION ptr = NULL;
+    DWORD returnLength = 0;
+    DWORD logicalProcessorCount = 0;
+    DWORD numaNodeCount = 0;
+    DWORD processorCoreCount = 0;
+    DWORD processorL1CacheCount = 0;
+    DWORD processorL2CacheCount = 0;
+    DWORD processorL3CacheCount = 0;
+    DWORD processorPackageCount = 0;
+    DWORD byteOffset = 0;
+    PCACHE_DESCRIPTOR Cache;
+
+    glpi = (LPFN_GLPI) GetProcAddress(
+                            GetModuleHandle(TEXT("kernel32")),
+                            "GetLogicalProcessorInformation");
+    if (NULL == glpi) 
+    {
+        //_tprintf(TEXT("\nGetLogicalProcessorInformation is not supported.\n"));
+        return (1);
+    }
+
+    while (!done)
+    {
+        DWORD rc = glpi(buffer, &returnLength);
+
+        if (FALSE == rc) 
+        {
+            if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) 
+            {
+                if (buffer) 
+                    free(buffer);
+
+                buffer = (PSYSTEM_LOGICAL_PROCESSOR_INFORMATION)malloc(
+                        returnLength);
+
+                if (NULL == buffer) 
+                {
+                    //_tprintf(TEXT("\nError: Allocation failure\n"));
+                    return (2);
+                }
+            } 
+            else 
+            {
+                //_tprintf(TEXT("\nError %d\n"), GetLastError());
+                return (3);
+            }
+        } 
+        else
+        {
+            done = TRUE;
+        }
+    }
+
+    ptr = buffer;
+
+    while (byteOffset + sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION) <= returnLength) 
+    {
+        switch (ptr->Relationship) 
+        {
+        case RelationNumaNode:
+            // Non-NUMA systems report a single record of this type.
+            numaNodeCount++;
+            break;
+
+        case RelationProcessorCore:
+            processorCoreCount++;
+
+            // A hyperthreaded core supplies more than one logical processor.
+            logicalProcessorCount += CountSetBits(ptr->ProcessorMask);
+            break;
+
+        case RelationCache:
+            // Cache data is in ptr->Cache, one CACHE_DESCRIPTOR structure for each cache. 
+            Cache = &ptr->Cache;
+            if (Cache->Level == 1)
+            {
+                processorL1CacheCount++;
+            }
+            else if (Cache->Level == 2)
+            {
+                processorL2CacheCount++;
+            }
+            else if (Cache->Level == 3)
+            {
+                processorL3CacheCount++;
+            }
+            break;
+
+        case RelationProcessorPackage:
+            // Logical processors share a physical package.
+            processorPackageCount++;
+            break;
+
+        default:
+            //_tprintf(TEXT("\nError: Unsupported LOGICAL_PROCESSOR_RELATIONSHIP value.\n"));
+            break;
+        }
+        byteOffset += sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION);
+        ptr++;
+    }
+
+/*
+    _tprintf(TEXT("\nGetLogicalProcessorInformation results:\n"));
+    _tprintf(TEXT("Number of NUMA nodes: %d\n"), 
+             numaNodeCount);
+    _tprintf(TEXT("Number of physical processor packages: %d\n"), 
+             processorPackageCount);
+    _tprintf(TEXT("Number of processor cores: %d\n"), 
+             processorCoreCount);
+    _tprintf(TEXT("Number of logical processors: %d\n"), 
+             logicalProcessorCount);
+    _tprintf(TEXT("Number of processor L1/L2/L3 caches: %d/%d/%d\n"), 
+             processorL1CacheCount,
+             processorL2CacheCount,
+             processorL3CacheCount);
+*/			 
+    
+    free(buffer);
+	
+	applog(LOG_INFO, "---------------------------Opredelili CPU");	
+	applog(LOG_INFO, "Number of processor cores: %d", processorCoreCount);
+	applog(LOG_INFO, "Number of logical processors: %d", logicalProcessorCount);
+	applog(LOG_INFO, "Total CPUs: %d", num_processors);
+	
+    return 0;
+}	
+	
+		_proccount();
+		
+
+	
+//-------------cpu indefinitely - END		
+	
 #elif defined(_SC_NPROCESSORS_CONF)
 	num_processors = sysconf(_SC_NPROCESSORS_CONF);
 #elif defined(CTL_HW) && defined(HW_NCPU)
